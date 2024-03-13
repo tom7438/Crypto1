def3a796bd1edd383109d0895704527234da6db4
tom7438 <tom192010@live.fr>
2024-03-13 16:46:18 +0100

    Nettoyage

--- smht48.c
+++ smht48.c
@@ -15,10 +15,10 @@ void smht48(const uint8_t k[static 6], uint64_t blen, const uint8_t m[blen], uin
     free(mk);
 }
 
-void smht48_test(){
+void smht48_test() {
     uint8_t h1[6];
     uint8_t h2[6];
-    uint8_t m[6] = {9, 8, 7, 6, 5 , 4};
+    uint8_t m[6] = {9, 8, 7, 6, 5, 4};
     uint8_t key1[6] = {0, 1, 2, 3, 4, 5};
     uint8_t key2[6] = {0xE4, 0x16, 0x9F, 0x12, 0xD3, 0xBA};
     uint64_t blen = sizeof(m);
@@ -32,5 +32,4 @@ void smht48_test(){
     smht48(key2, blen, m, h2);
     printhash(h2);
     printf(" (Attendue : 5F265B72B5EC)\n");
-
 }
